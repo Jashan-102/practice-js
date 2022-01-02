61f9bf42d8ce5c0e4c2e467e10e8c1c2d4cb0827
Jashandeep <jashandeepk22000@gmail.com>
2022-01-02 18:33:57 +0530

    added README.md

---
+++ 01.js
@@ -0,0 +1,2 @@
+console.log("Hii, I am learning JS");
+console.log("Date: 31/12/2021");
---
+++ string-methods.js
@@ -0,0 +1,9 @@
+console.log("String Methods:");
+
+let firstName = "Jashandeep Kaur";
+let age = 21;
+
+
+
+// console.log (firstName.length);
+console.log (firstName.charAt(4));
