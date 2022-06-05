5f47061c86c683b3cbc36529338d120d7c744720
Jashandeep <jashandeepk22000@gmail.com>
2022-06-05 15:26:09 +0530

    Promises added

--- js/AsyncJS.js
+++ js/AsyncJS.js
@@ -116,4 +116,168 @@ console.log(
 
 console.log("Promises status:\n1. Pending\n2. Fullfilled\n3. Rejected");
 
-console.log("Create Promises OR Produce Promises:\n");
+console.log(
+  "Create Promises OR Produce Promises:\nPromise took two parameters:\n1. resolve-Its is a method you can use when a promise is fulfilled.\n2. reject\nwhich are also functions.\nPromise constructor is used to create a Promise."
+);
+
+const bucket = ["vegetables", "rice", "salt"];
+
+const friedRice = new Promise((resolve, reject) => {
+  if (bucket.includes("no")) {
+    resolve("Fried Rice");
+  } else {
+    reject("No Fried Rice");
+  }
+});
+console.log(friedRice);
+
+console.log(
+  "How to use Promise OR Consume Promise:\nAs JS developer You consume Promise more than Produce.\nHow to consume Promise is important than Produce:\n"
+);
+
+console.log(
+  ".then is a method.\n.then took 2 Callbacks.\n1. First one is for resolved O/P.\n2. Second one is for error/rejected O/P [optional]"
+);
+friedRice.then(
+  (res) => {},
+  (err) => {}
+);
+
+friedRice.then(
+  (res) => {
+    console.log("Lets eat", res);
+  },
+  (err) => {
+    const error = new Error(err);
+    console.log(error);
+  }
+);
+
+console.log("You can also use Error Constructor for error message");
+
+console.log(
+  "error callback in .then method is optional\nInstead of it we use .catch method."
+);
+console.log(
+  ".catch method: It placed after .then method which lead to catch error Or handle reject case."
+);
+
+friedRice
+  .then((res) => {
+    console.log("Lets eat", res);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+console.log(
+  "Promises works Asynchronously.As Promise task assigned to or handle by browser.\nPromise is a object having 2 values in it.\n{state: , value: }"
+);
+console.log(friedRice);
+
+console.log(
+  "Promise Async Description:\n1. Firstly Promise Produce and Promise object created.\n2. Promise consume task is being assigned to or handle by browser. Browser and Promise objects in memory having connection with each other to perform tasks.\n3. When Promise is resolved by browser it will give .then method to MicrotaskQueue. If rejected .catch method will be place in MicrotaskQueue. All the promises wait in MicrotaskQueue and eventLoop handle their execution. When the callStack empty, eventLoop assign that task to callStack for execution."
+);
+
+console.log(
+  "Promise and setTimeout:\nMicrotaskQueue has high priority.\nIf their is task present in callback Queue and in MicrotaskQueue:\nFirstly MicrotaskQueue execution performed."
+);
+
+console.log("Function returning Promise:\n");
+
+function myPromise() {
+  const name = ["Jashandeep", "Maninder", "Arsh"];
+  return new Promise((resolve, reject) => {
+    if (name.includes("Arsh")) {
+      resolve("Yes");
+    } else {
+      reject("Not");
+    }
+  });
+}
+
+// myPromise()
+//   .then((res) => {
+//     return console.log(res, "Present");
+//   })
+//   .catch((err) => {
+//     return console.log(err, "Present");
+//   });
+
+console.log("Promise and setTimeout:");
+function myPromise2() {
+  const name2 = ["Jashandeep", "Maninder", "Arsh"];
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (name2.includes("Arsh")) {
+        resolve("2 Yes");
+      } else {
+        reject("2 Not");
+      }
+    }, 2000);
+  });
+}
+
+myPromise2()
+  .then((res) => {
+    return console.log(res, "Present");
+  })
+  .catch((err) => {
+    return console.log(err, "Present");
+  });
+
+console.log(
+  "Promise resolve and Promise chaining:\n.then method always return a Promise. You can also store this in a variable to check."
+);
+
+const isPromiseMyPromise2 = Promise.resolve("abc");
+
+console.log(
+  "Promise always return something. If you not mention return O/P will be undefined.\nReturned value is equal to\nPromise.resolve('value')\nThis has returned promise to us now we can use .then menthod"
+);
+
+console.log("Promise chaining means using .then method again and again");
+myPromise()
+  .then((res) => {
+    console.log(res);
+    return res;
+  })
+  .then((res) => {
+    console.log(res);
+    return res;
+  })
+  .then((res) => {
+    console.log(res);
+    return res;
+  })
+  .then((res) => {
+    console.log(res);
+    return res;
+  });
+
+console.log(
+  "Callback hell to flat Code:Callback hell can be solve using Promises"
+);
+
+function changeText2(element, text, time) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (element) {
+        element.textContent = text;
+        resolve();
+      } else {
+        reject();
+      }
+    }, time);
+  });
+}
+
+changeText2(headings[0], "One", 1000)
+  .then(() => changeText2(headings[1], "Two", 1000))
+  .then(() => changeText2(headings[2], "Three", 1000))
+  .then(() => changeText2(headings[3], "Four", 1000))
+  .then(() => changeText2(headings[4], "Five", 1000))
+  .then(() => changeText2(headings[5], "Six", 1000))
+  .then(() => changeText2(headings[6], "Seven", 1000))
+  .then(() => changeText2(headings[7], "Eight", 1000))
+  .catch(() => alert("Error"));
